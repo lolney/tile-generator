859eb83fc094c12b5e56988bced17fd6c304d1ca
Edward Olney-Bell <eolneybell@gmail.com>
2020-05-12 17:31:26 -0700

    Update attribution and FAQ

--- packages/react-app/src/pages/About/index.tsx
+++ packages/react-app/src/pages/About/index.tsx
@@ -50,9 +50,15 @@ export const About: React.FC = () => (
       </div>
       <div className={styles.bottom_bar}>
         <div className={styles.outro}>
-          <h1 className={styles.lower_landing_header}>Attributions</h1>
-          <p className={styles.lower_landing_subtitle}>
-            Climate and terrain data used to generate these maps
+          <h1 className={styles.lower_landing_header}>Attribution</h1>
+          <p className={styles.attribution_header}>Climate data</p>
+          <p className={styles.paragraph}>
+            Beck, H., Zimmermann, N., McVicar, T. et al. Present and future
+            Köppen-Geiger climate classification maps at 1-km resolution. Sci
+            Data 5, 180214 (2018). https://doi.org/10.1038/sdata.2018.214
+          </p>
+          <p className={styles.attribution_header}>
+            Water (lake, coast, ocean) data
           </p>
           <p className={styles.paragraph}>
             Carroll, M.L., DiMiceli, C.M., Wooten, M.R., Hubbard, A.B.,
@@ -61,22 +67,26 @@ export const About: React.FC = () => (
             [Data set]. NASA EOSDIS Land Processes DAAC. Accessed 2020-05-11
             from https://doi.org/10.5067/MODIS/MOD44W.006
           </p>
+          <p className={styles.attribution_header}>Marsh/wetlands data</p>
           <p className={styles.paragraph}>
             Friedl, M., Sulla-Menashe, D. (2019). MCD12Q1 MODIS/Terra+Aqua Land
             Cover Type Yearly L3 Global 500m SIN Grid V006 [Data set]. NASA
             EOSDIS Land Processes DAAC. Accessed 2020-05-11 from
             https://doi.org/10.5067/MODIS/MCD12Q1.006
           </p>
+          <p className={styles.attribution_header}>Elevation data</p>
           <p className={styles.paragraph}>
             Jarvis, A., H.I. Reuter, A. Nelson, E. Guevara. 2008. Hole-filled
             SRTM for the globe Version 4, available from the CGIAR-CSI SRTM 90m
             Database: http://srtm.csi.cgiar.org.
           </p>
+          <p className={styles.attribution_header}>River data</p>
           <p className={styles.paragraph}>
             Lehner, B., Verdin, K., Jarvis, A. (2008): New global hydrography
             derived from spaceborne elevation data. Eos, Transactions, AGU,
             89(10): 93-94.
           </p>
+          <p className={styles.attribution_header}>Forest data</p>
           <p className={styles.paragraph}>
             Masanobu Shimada, Takuya Itoh, Takeshi Motooka, Manabu Watanabe,
             Shiraishi Tomohiro, Rajesh Thapa, and Richard Lucas, "New Global
--- packages/react-app/src/pages/Help/index.tsx
+++ packages/react-app/src/pages/Help/index.tsx
@@ -108,11 +108,17 @@ const FAQ: React.FC = () => (
           Civilization VI?
         </h2>
         <p className={styles.answerText}>
-          You may be experiencing a novel issue, but it is also likely that you
-          have added too many city states to your game. Currently the maps
-          generated here support only 1.5x the maximum number of civilizations
-          for a given map size. For example, a small map can support up to 6
-          civilizations, so the maximum number of cities states would be 9.
+          This is likely related to a known bug in Civilization VI involving
+          starting positions on custom maps (you can read more about it{" "}
+          <a href="https://github.com/lolney/tile-generator/issues">here</a>).
+          As a result, Civilization Tile Generator imposes a limit on the number
+          of city states per game, which is 1.5x the maximum number of
+          civilizations on a given map size. For example, a small map can
+          support up to 6 civilizations, so the maximum number of cities states
+          would be 9. Adding more than 9 city states in this case will generate
+          and error and the game will fail to load. Until Civilization VI is
+          patched or Civilization Tile Generator devises a better way to handle
+          the bug, this problem will persist.
         </p>
         <h2 className={styles.questionHeader}>Where can I report issues?</h2>
         <p className={styles.answerText}>
